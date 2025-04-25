6873592f14a857beb39e59121b31ece380dcd15a
Gabriel Erick <gabrielmmve13@gmail.com>
2025-04-25 17:45:51 -0300

    criando projeto novamente

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.admin')
-
-@section('conteudo')
-    <div class="d-flex justify-content-between mt-3">
-        <h2>Dashboard</h2>
-        <p>Área Administrativa</p>
-    </div>
-@endsection
