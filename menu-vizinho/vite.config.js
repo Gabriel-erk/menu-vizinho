e747a4403f93a952a3fdd2b2a6ed3e85774b8394
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-11 11:29:40 -0300

    corrigindo problemas de scss

--- menu-vizinho/vite.config.js
+++ menu-vizinho/vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import * as path from 'path';  // <-- Importa o módulo 'path' pois é necessário para realizar as alterações dos elementos definidos do bootstrap
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
         tailwindcss(),
     ],
+    // !! necessário esse alias para que eu consiga acessar as váriaveis do bootstrap e modifica-las
+    resolve: {
+        alias: {
+            // Alias para o diretório do Bootstrap SCSS
+            "@bootstrap": path.resolve(
+                __dirname,
+                "node_modules/bootstrap/scss"
+            ),
+        },
+    },
 });
