184f5c1fd8644278b9e5502d07ebd5992812361a
Gabriel Erick <gabrielmmve13@gmail.com>
2025-05-27 19:53:32 -0300

    finalizando tela de login para mobile

--- menu-vizinho/resources/views/login.blade.php
+++ menu-vizinho/resources/views/login.blade.php
@@ -31,7 +31,27 @@
             </div>
         </form>
 
-        
+        <div class="mt-4 d-flex align-items-center gap-2">
+            {{-- hr cria uma linha horizontal, flex-grow-1 faz ele ocupar todo o espaço disponível (no caso, todo o espaço a esquerda, e o outro depois do p "ou" ocupa todo o espaço a direita, o gap na div pai é para separar essas 3 tags uma da outra) --}}
+            {{-- ? mb-0 e m-0 e para remover o espaçamento padrão que essas tags tem --}}
+            <hr class="flex-grow-1 m-0">
+            <p class="mb-0">Ou</p>
+            <hr class="flex-grow-1 m-0">
+        </div>
+
+        <div class="mt-4 d-flex flex-column gap-3">
+
+            <a href="#" class="login-social-media btn btn-secondary d-flex justify-content-center align-items-center gap-2 py-2">
+                <img src="{{ asset('assets/login/google_icon.svg') }}" alt="ícone do Google">
+                <p class="text-light m-0">Entrar com Google</p>
+            </a>
+
+            <a href="#" class="login-social-media btn btn-secondary d-flex justify-content-center align-items-center gap-2 py-2">
+                <img src="{{ asset('assets/login/Facebook_Logo_Primary.png') }}" alt="ícone do Google">
+                <p class="text-light m-0">Entrar com Facebook</p>
+            </a>
+            
+        </div>
 
     </div>
 </x-layout>
